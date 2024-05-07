433628ada548861262a95f80ac72257e7d82acdd
antonpichka <antonpichka@gmail.com>
2024-05-07 13:11:42 +0300

    refactoring(.): upd

--- api_template_for_lamm_javascript/routes.js
+++ api_template_for_lamm_javascript/routes.js
@@ -1,54 +1,120 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/user/:id", (req, res) => {
-  const id = req.params.id;
-  if(id != 1) {
-    res.json({
-      error: "Not found id, user"
-    });
-    return;
-  }
-  res.json({
-    id: 1,
-    name: "Qwerty",
-    email: "qwerty@example.com"
-  });
-});
-
-router.get("/other", (req, res) => {
-  res.json({
-    name: "Qwerty",
-  });
-});
-
 router.post("/login", (req,res) => {
-  const header = req.header("Authorization");
-  const token = header != null ? header.replace("Bearer ", "") : null;
-  const body = req.body;
-  const username = body.username ?? "";
-  const password = body.password ?? "";
-  if(token == null) {
-    res.json({  
-      error: "You are not logged in"
+  const authorization = req.header("Authorization") == null ? null : req.header("Authorization").replace("Bearer ", "");
+  const username = req.body.username == null || req.body.username == "" ? null : req.body.username;
+  const password = req.body.password == null || req.body.password == "" ? null : req.body.password;
+  if(authorization == null) {
+    res.status(401).json({
+      error: "401 (Unauthorized) You must specify 'Authorization'"
     });
     return;
   } 
-  if(token != "f434343fwesferrrewsqswqwwrewdwq1") {
-    res.json({  
-      error: "Token is not correct"
+  if(authorization != "f434343fwesferrrewsqswqwwrewdwq1") {
+    res.status(401).json({  
+      error: "401 (Unauthorized) Token is not correct."
     });
     return;
   }
-  if(username == "" || password == "") {
-    res.json({  
-      error: "Not found is body data, for insert user in database"
+  if(username == null || password == null) {
+    res.status(400).json({  
+      error: "400 (Bad Request) The request could not be understood by the server due to malformed syntax."
     });
     return;
   }
-  res.json({
-    username : username,
-    password : password
+  if(username == "qwe") {
+    res.status(409).json({  
+      error: "409 (Conflict) Such data is already in the database."
+    });
+    return;
+  }
+  res.status(200).json({});
+});
+
+router.get("/search", (req, res) => {
+  const authorization = req.header("Authorization") == null ? null : req.header("Authorization").replace("Bearer ", "");
+  const q = req.query.q == null ? null : req.query.q;
+  if(authorization == null) {
+    res.status(401).json({
+      error: "401 (Unauthorized) You must specify 'Authorization'"
+    });
+    return;
+  } 
+  if(authorization != "f434343fwesferrrewsqswqwwrewdwq1") {
+    res.status(401).json({  
+      error: "401 (Unauthorized) Token is not correct."
+    });
+    return;
+  }
+  if(q == null) {
+    res.status(400).json({  
+      error: "400 (Bad Request) The request could not be understood by the server due to malformed syntax."
+    });
+    return;
+  }
+  if(q.charAt(0) != "q") {
+    res.status(200).json({
+    });
+    return;
+  }
+  res.status(200).json({
+    users : [
+      {
+        username: "q"
+      },
+      { 
+        username: "qw",
+      },
+    ]
+  });
+});
+
+
+router.get("/other", (req, res) => {
+  const authorization = req.header("Authorization") == null ? null : req.header("Authorization").replace("Bearer ", "");
+  if(authorization == null) {
+    res.status(401).json({
+      error: "401 (Unauthorized) You must specify 'Authorization'"
+    });
+    return;
+  } 
+  if(authorization != "f434343fwesferrrewsqswqwwrewdwq1") {
+    res.status(401).json({  
+      error: "401 (Unauthorized) Token is not correct."
+    });
+    return;
+  }
+  res.status(200).json({
+    name: "Qwerty",
+  });
+});
+
+router.get("/user/:id", (req, res) => {
+  const authorization = req.header("Authorization") == null ? null : req.header("Authorization").replace("Bearer ", "");
+  const id = req.params.id;
+  if(authorization == null) {
+    res.status(401).json({
+      error: "401 (Unauthorized) You must specify 'Authorization'"
+    });
+    return;
+  } 
+  if(authorization != "f434343fwesferrrewsqswqwwrewdwq1") {
+    res.status(401).json({  
+      error: "401 (Unauthorized) Token is not correct."
+    });
+    return;
+  }
+  if(id != 1) {
+    res.status(404).json({
+      error: "404 (Not Found) Not found id, user"
+    });
+    return;
+  }
+  res.status(200).json({
+    id: 1,
+    name: "Qwerty",
+    email: "qwerty@example.com"
   });
 });
 
--- website_template_for_lamm_javascript/routes.js
+++ website_template_for_lamm_javascript/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+// const { OtherRedirect } = require("./public/src/named_utility/named_redirect/other_redirect");
 
-router.get("/", (req, res) => {
+router.get("/", (_req, res) => {
   res.sendFile(__dirname + "/public/src/named_vm/index_vm/index_view.html");
 });
 
-router.get("/other", (req, res) => {
+router.get("/other", (_req, res) => {
+  // Using "OtherRedirect" if needs
   res.sendFile(__dirname + "/public/src/named_vm/other_vm/other_view.html");
 });
 
