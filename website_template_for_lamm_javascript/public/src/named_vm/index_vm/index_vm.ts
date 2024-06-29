90d640e69a87ee3dbda1bb91699b2db71a805dbf
antonpichka <antonpichka@gmail.com>
2024-06-29 16:58:56 +0300

    refactoring(.): release v3.0.1

--- website_template_for_lamm_javascript/public/src/named_vm/index_vm/index_vm.ts
+++ website_template_for_lamm_javascript/public/src/named_vm/index_vm/index_vm.ts
@@ -29,18 +29,10 @@ class IndexVM {
         const dataForNamed = this.namedStreamWState.getDataForNamed;
         switch(dataForNamed.getEnumDataForNamed) {
             case EnumDataForIndexVM.isLoading:
-                document.getElementById("loader").style.display = "block";
-                document.getElementById("pMessage").hidden = true;
                 break;
             case EnumDataForIndexVM.exception:
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("pMessage").hidden = false;
-                document.getElementById("pMessage").innerText = dataForNamed.exceptionController.getKeyParameterException;
                 break;
             case EnumDataForIndexVM.success:
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("pMessage").hidden = false;
-                document.getElementById("pMessage").innerText = "IndexVM";
                 break;
             default:
                 break;      
