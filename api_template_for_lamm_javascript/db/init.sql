944cf6e2a4801b4c07fc667a02e323d7c680f500
antonpichka <antonpichka@gmail.com>
2024-07-08 12:41:20 +0300

    refactoring(.): upd

--- api_template_for_lamm_javascript/db/init.sql
+++ api_template_for_lamm_javascript/db/init.sql
@@ -13,5 +13,5 @@ CREATE TABLE users (
     CONSTRAINT UQ_Users_UniqueId UNIQUE (unique_id)
 );
 
-INSERT INTO users (unique_id, username) VALUES ("9ac307f5-f85b-4b45-a990-743fa0d1e59c", "q");
-INSERT INTO users (unique_id, username) VALUES ("8c23f644-58af-4be5-abcb-5ffe2982fa99", "qw");
+INSERT INTO users (unique_id, username) VALUES ('9ac307f5-f85b-4b45-a990-743fa0d1e59c', 'q');
+INSERT INTO users (unique_id, username) VALUES ('8c23f644-58af-4be5-abcb-5ffe2982fa99', 'qw');
